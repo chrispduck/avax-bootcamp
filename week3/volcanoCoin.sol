b5fe6217f9fc025bbfac54a9133cd8be0e4fb680
"chrispduck" <"chrisjamesparsons@hotmail.co.uk">
2021-11-15 19:30:56 +0000

    try out openzepplin

---
+++ week3/volcanoCoin.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED .
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts@4.2.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.2.0/access/Ownable.sol";
+
+contract VolcanoCoin is ERC20("ChrisCoin", "CCO"), Ownable{
+    constructor(){
+        _mint(msg.sender, 10000);
+    }
+    
+    function mintTokens(uint256 _amount) public onlyOwner{
+        _mint(owner(), _amount);
+    }
+    
+    struct funder {
+        address dest;
+        uint256 amount;
+    }
+    mapping (address => funder[]) public payment; 
+
+    function increaseTotalSupply(uint256 _amount) public onlyOwner {
+         _mint(owner(), _amount);
+    }
+    
+    function send(uint256 _amount, address _destination) public {
+        _transfer(msg.sender, _destination, _amount);
+        payment[msg.sender].push(funder({dest: _destination, amount: _amount}));
+    }
+}
