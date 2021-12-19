bf3135362573d970ad62aaeedb967e70d76f3611
Chris Parsons <chrisjamesparsons@hotmail.co.uk>
2021-12-19 18:27:29 +0200

    add contract and start tests

--- week7/DeFiHardhat/test/sample-test.js
+++ week7/DeFiHardhat/test/sample-test.js
@@ -12,29 +12,42 @@ describe("DeFi", () => {
   let DAI_TokenContract;
   let USDC_TokenContract;
   let DeFi_Instance;
+  let whale;
   const INITIAL_AMOUNT = 999999999000000;
   before(async function () {
     [owner, addr1, addr2, addr3, addr4, addr5] = await ethers.getSigners();
+
+    // Get relevant token contracts
+    DAI_TokenContract = await ethers.getContractAt("ERC20", DAIAddress);
+    USDC_TokenContract = await ethers.getContractAt("ERC20", USDCAddress);
+    console.log(await DAI_TokenContract.symbol());
+    
+    // Tack control of the whale
+    // Check that we have control of the whale account
     const whale = await ethers.getSigner(
       "0x503828976D22510aad0201ac7EC88293211D23Da"
     );
-    console.log("owner account is ", owner.address);
 
-    DAI_TokenContract = await ethers.getContractAt("ERC20", DAIAddress);
-    USDC_TokenContract = await ethers.getContractAt("ERC20", USDCAddress);
-    const symbol = await DAI_TokenContract.symbol();
-    console.log(symbol);
+    // Deploy our contract
     const DeFi = await ethers.getContractFactory("DeFi");
+    DeFi_Instance = await DeFi.deploy();
+  });
 
+  it("should check transfer succeeded", async () => {
+    let initialBalance = await DAI_TokenContract.balanceOf(owner.address);
     await DAI_TokenContract.connect(whale).transfer(
       owner.address,
       BigInt(INITIAL_AMOUNT)
     );
-
-    DeFi_Instance = await DeFi.deploy();
+    expect(await DAI_TokenContract.balanceOf(owner.address)).to.equal(initialBalance + INITIAL_AMOUNT);
   });
 
-  it("should check transfer succeeded", async () => {});
-  it("should sendDAI to contract", async () => {});
-  it("should make a swap", async () => {});
+  it("should sendDAI to contract", async () => {
+    expect(await DAI_TokenContract.transfer(DeFi_Instance.address, 99999999900000)).to.equal(true);
+    
+  });
+
+  it("should make a swap", async () => {
+    expect
+  });
 });
