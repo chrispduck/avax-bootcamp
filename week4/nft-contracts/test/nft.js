e441ec6567feb4ba013be86ba73057cca019f7ac
Chris Parsons <chrisjamesparsons@hotmail.co.uk>
2021-12-22 18:49:53 +0200

    add nft test

--- week4/nft-contracts/test/nft.js
+++ week4/nft-contracts/test/nft.js
@@ -1,16 +1,17 @@
 const { expect, assert } = require("chai");
 const { ethers } = require("hardhat");
+const metadata = require("../metadata/cheetah-nft.json")
 
-describe("volcanoToken", function () {
-  it("Should create token and check the URI", async function () {
-    const VolcanoToken = await ethers.getContractFactory("volcanoToken");
-    const volcanoToken = await VolcanoToken.deploy();
-    await volcanoToken.deployed();
-    address = "0xb794f5ea0ba39494ce839613fffba74279579268";
-    uri = "myURI"
-    tokenID = 1
-    await volcanoToken.mint(address, uri);
-    console.log(typeof((await volcanoToken.balanceOf(address))._hex));
-    assert.equal(await volcanoToken.tokenURISimple(1), uri);
-  });
+describe("nft", function (){
+  before(async function() {
+    [owner, addr1] = await ethers.getSigners();
+    let factory = await ethers.getContractFactory("nft",);
+    nft = await factory.deploy();
+  })
+
+  it("can mint a token", async () => {
+    let tx = await nft.mint(addr1, metadata);
+    let result = await tx.wait();
+    console.log(`result ${result}`)
+  })
 });
