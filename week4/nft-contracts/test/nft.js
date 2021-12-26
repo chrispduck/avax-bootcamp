b7712c2e3c00d0e041b4dcee3bd9f68e98d170f6
Chris Parsons <chrisjamesparsons@hotmail.co.uk>
2021-12-26 17:13:57 +0200

    add transfer test

--- week4/nft-contracts/test/nft.js
+++ week4/nft-contracts/test/nft.js
@@ -4,7 +4,7 @@ const metadata = require("../metadata/cheetah-nft.json");
 
 describe("nft", function (){
   before(async function() {
-    [owner, addr1] = await ethers.getSigners();
+    [owner, addr1, addr2] = await ethers.getSigners();
     let factory = await ethers.getContractFactory("nft",);
     nft = await factory.deploy();
     console.log("metadata: ", JSON.stringify(metadata));
@@ -25,4 +25,12 @@ describe("nft", function (){
   it("can retrieve metadata", async () => {
     expect(await nft.tokenURI(1)).to.equal(JSON.stringify(metadata));
   })
+
+  it("can transfer", async () => {
+    let tx = await nft.connect(addr1).transferFrom(addr1.address, addr2.address, 1);
+    await tx.wait();
+    expect(await nft.ownerOf(1)).to.equal(addr2.address);
+  })
+
+  
 });
